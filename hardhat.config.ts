7a151630c7b2d933ddd9c769214becc5a1d46d17
jeissoni <jeissoni_22@hotmail.com>
2022-05-12 18:10:00 -0500

    add network and parameter for deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,13 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+import "@nomiclabs/hardhat-etherscan";
+
+require('dotenv').config()
+
+const projeId=process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const etherscanApi = process.env.ETHERSCAN_API_KEY;
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,14 +16,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+
+  networks:{
+
+    rinkeby:{
+      url:`https://rinkeby.infura.io/v3/${projeId}`,
+      accounts:[
+        privateKey
+      ]
+    }
+  },
+
+  etherscan: {
+    apiKey: etherscanApi
+  },
+
   settings: {
     optimizer: {
       enabled: true,
